c3a450b70e0b673218591d4bd6cd1352ee267c19
Kutay <kutay.ugurlu.1@gmail.com>
2022-06-13 15:15:00 +0300

    now everything works. when the plots finish, imshow the best !

--- sart_reconstruct.m
+++ sart_reconstruct.m
@@ -1,4 +1,4 @@
-function [IMAGE, errors] = sart_reconstruct(Image_size, A, if_relax, n_iter, PROJECTIONS, Original_Image, if_show, patience)
+function [IMAGE, errors] = sart_reconstruct(Image_size, A, if_relax, n_iter, PROJECTIONS, Original_Image, if_show, patience, axes)
 
 if if_relax
     alpha = 1.95/norm(A'*A,"fro");
@@ -11,11 +11,11 @@ PROJECTIONS = PROJECTIONS(:);
 f = zeros(Image_size^2,1);
 % L_proj = size(A,1);
 errors = zeros(1,n_iter);
-for i = 1:n_iter
+for iter = 1:n_iter
     f = f - alpha*A'*(A*f-PROJECTIONS);
     f(f<0) = 0;
     IMAGE = (reshape(f,Image_size,Image_size));
-    errors(i) = reconstruction_error(Original_Image ,IMAGE);
+    errors(iter) = reconstruction_error(Original_Image ,IMAGE);
     
     if errors(end) == min(errors)
         IMAGE_best = IMAGE;
@@ -29,12 +29,12 @@ for i = 1:n_iter
     end
     
     if if_show 
-        imagesc(axes,IMAGE); colormap(axes,"gray")
+        imagesc(axes,imrotate(IMAGE,90)); colormap(axes,"gray")
         title(axes,{['Iteration ',num2str(iter),' completed.']})
         hold on 
         drawnow
     end
-    
+
 end
 errors = errors(1:find(errors,1,'last'));
 end
